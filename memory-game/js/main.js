2249f3b0d4b9dec7029b76aded92e8e70f7e1fe8
kay <kaych26@gmail.com>
2020-02-06 14:20:06 -0500

    final game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -4,46 +4,75 @@ let cards = [
 	{
 	rank: "queen",
 	suit: "hearts", 
-	ardImage: "images/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png"
 	},
 	{
 	rank: "queen",
 	suit: "diamonds", 
-	ardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png"
 	},
 	{
 	rank: "king",
 	suit: "hearts", 
-	ardImage: "images/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png"
 	},
 	{
 	rank: "king",
 	suit: "diamonds", 
-	ardImage: "images/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png"
 	}
 
 ]
 let cardsInPlay = [];
+let score=0;
+
 
 function checkForMatch() {
+
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		score += 1;
+		document.getElementById('score-box').innerHTML=score;
+
+		alert("You found a match!");
 	}
 	else {
 		alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId=this.getAttribute('data-id');
 	console.log ("User flipped " + cards[cardId].rank);
 	console.log ("User flipped " + cards[cardId].suit);
-	console.log ("User flipped " + cards[cardId].ardImage);
+	console.log ("User flipped " + cards[cardId].cardImage);
 
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+	for (let i=0; i<cards.length; i++) {
+	
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+function resetBoard() {
+	console.log("Reset game board");
+	cardsInPlay=[];
+
+	// clear board & create board with face down
+	document.getElementById('game-board').innerHTML="";
+	createBoard();
+}
+
+createBoard();
+document.getElementById('reset-But').addEventListener('click', resetBoard);
